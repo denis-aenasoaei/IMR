a170ede3a64d376eff07444b065551251faacc2f
denis-aenasoaei <denis.aenasoaei@yahoo.com>
2020-10-08 00:01:31 +0300

    added VR Hello world with animation

---
+++ AR Test/Assets/AnimSpaceman.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimSpaceman : MonoBehaviour
+{
+    public Animator animator ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AR Test/Assets/FreeAnimatedSpaceMan/Models/PlayAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    public Animator animator = new Animator(); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s") && animator.GetCurrentAnimatorStateInfo(0).IsName("Default State"))
+        {
+            animator.Play("Animation");
+        }
+        else if (Input.GetKeyDown("s") && animator.GetCurrentAnimatorStateInfo(0).IsName("Animation"))
+        {
+            animator.Play("Default State");
+        }
+    }
+}
